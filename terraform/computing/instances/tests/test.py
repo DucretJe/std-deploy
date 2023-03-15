fa9dcb3b50423e35af50c65200099d35e46b9795
DucretJe <jdiphone34@gmail.com>
2023-03-15 20:04:20 +0100

    Lint python

--- terraform/computing/instances/tests/test.py
+++ terraform/computing/instances/tests/test.py
@@ -1,18 +1,25 @@
 import argparse
+import time
+
 import boto3
 import requests
-import time
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--region", required=True, help="AWS region")
-    parser.add_argument("--launch-template-id", required=True, help="Launch Template ID")
-    parser.add_argument("--autoscaling-group-id", required=True, help="Auto Scaling Group ID")
+    parser.add_argument(
+        "--launch-template-id", required=True, help="Launch Template ID"
+    )
+    parser.add_argument(
+        "--autoscaling-group-id", required=True, help="Auto Scaling Group ID"
+    )
     parser.add_argument("--url", required=True, help="URL to check")
     return parser.parse_args()
 
+
 class TestFailed(Exception):
     pass
+
 
 def test_launch_template_exists(region_name, launch_template_id):
     try:
@@ -24,11 +31,16 @@ def test_launch_template_exists(region_name, launch_template_id):
         )
     except ec2_client.exceptions.ClientError as e:
         if e.response["Error"]["Code"] == "InvalidLaunchTemplateId.NotFound":
-            raise TestFailed(f"Launch template with ID {launch_template_id} does not exist")
+            raise TestFailed(
+                f"Launch template with ID {launch_template_id} does not exist"
+            )
         else:
             raise TestFailed(f"An error occurred: {e}")
 
-    assert len(response["LaunchTemplates"]) == 1, f"Launch template with ID {launch_template_id} does not exist"
+    assert (
+        len(response["LaunchTemplates"]) == 1
+    ), f"Launch template with ID {launch_template_id} does not exist"
+
 
 def test_autoscaling_group_exists(region_name, autoscaling_group_id):
     try:
@@ -41,7 +53,10 @@ def test_autoscaling_group_exists(region_name, autoscaling_group_id):
     except autoscaling_client.exceptions.ClientError as e:
         raise TestFailed(f"An error occurred: {e}")
 
-    assert len(response["AutoScalingGroups"]) == 1, f"Auto Scaling Group with ID {autoscaling_group_id} does not exist"
+    assert (
+        len(response["AutoScalingGroups"]) == 1
+    ), f"Auto Scaling Group with ID {autoscaling_group_id} does not exist"
+
 
 def test_url_status_code(url, retries=10, delay=5):
     for attempt in range(retries):
@@ -53,9 +68,12 @@ def test_url_status_code(url, retries=10, delay=5):
             pass
 
         if attempt == retries - 1:
-            raise TestFailed(f"Failed to make request to http://{url}:80 after {retries} retries")
+            raise TestFailed(
+                f"Failed to make request to http://{url}:80 after {retries} retries"
+            )
 
         time.sleep(delay)
+
 
 def run_test(test_func, test_args):
     try:
@@ -65,6 +83,7 @@ def run_test(test_func, test_args):
         print(f"ERROR: {e}")
         return False
     return True
+
 
 if __name__ == "__main__":
     args = parse_args()
