7a5717878c932e33a65d586604dda1e84fcc98c5
DucretJe <jdiphone34@gmail.com>
2023-04-09 16:09:50 +0200

    providers

--- terraform/computing/instances/tests/01-network/providers.tf
+++ terraform/computing/instances/tests/01-network/providers.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.62.0"
+    }
     random = {
       source  = "hashicorp/random"
       version = "3.4.3"
