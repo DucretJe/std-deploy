7de161bd9261123acb4a4280635a5030e98afab5
Jerome Ducret <5384298+DucretJe@users.noreply.github.com>
2023-04-10 16:27:59 +0200

    Add infracost (#37)
    * Add worfklow
    
    * Fix providers in tests/02-instances
    
    * Use matrix
    
    * Lint
    
    * fix matrix
    
    * lint
    
    * fix indent
    
    * add usage
    
    * remove in usage in post
    
    * test
    
    * test
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * debug
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * WIP
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * lint
    
    * wip
    
    * wip
    
    * lint

--- terraform/computing/instances/tests/02-instances/providers.tf
+++ terraform/computing/instances/tests/02-instances/providers.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.62.0"
+    }
     http = {
       source  = "hashicorp/http"
       version = "3.2.1"
