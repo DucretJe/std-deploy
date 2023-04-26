4249397835021588e4ba734c912d8718324d2fe3
DucretJe <jdiphone34@gmail.com>
2023-04-26 19:19:02 +0200

    Add terraform/computing/eks/aws
    * Spawn a public EKS cluster with nodes

---
+++ terraform/computing/eks/aws/eks.tf
@@ -0,0 +1,55 @@
+resource "aws_eks_cluster" "this" {
+  name     = var.eks_cluster_name
+  role_arn = aws_iam_role.eks_role.arn
+
+  vpc_config {
+    subnet_ids              = var.eks_cluster_subnets_ids
+    endpoint_private_access = true
+    endpoint_public_access  = true
+    security_group_ids = [
+      aws_security_group.eks.id,
+      aws_security_group.worker_nodes.id,
+    ]
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.AmazonEKSVPCResourceController,
+  ]
+}
+
+data "aws_iam_policy_document" "assume_eks_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "eks_role" {
+  name               = "eks-cluster-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_eks_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_role.name
+}
+
+# Optionally, enable Security Groups for Pods
+# Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
+resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks_role.name
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = aws_eks_cluster.this.name
+}
---
+++ terraform/computing/eks/aws/nodes.tf
@@ -0,0 +1,63 @@
+data "aws_ssm_parameter" "eks_ami_release_version" {
+  name = "/aws/service/eks/optimized-ami/${aws_eks_cluster.this.version}/amazon-linux-2/recommended/release_version"
+}
+
+resource "aws_eks_node_group" "this" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = var.eks_cluster_name
+  node_role_arn   = aws_iam_role.eks_workers.arn
+  version         = aws_eks_cluster.this.version
+  release_version = nonsensitive(data.aws_ssm_parameter.eks_ami_release_version.value)
+  subnet_ids      = var.eks_cluster_subnets_ids
+  capacity_type   = var.eks_worker_capacity_type
+
+  scaling_config {
+    desired_size = var.eks_workers_desired_capacity
+    max_size     = var.eks_workers_max_capacity
+    min_size     = var.eks_workers_min_capacity
+  }
+
+  update_config {
+    max_unavailable = var.eks_workers_max_unavailable
+  }
+
+  tags = {
+    for key, value in var.eks_group_nodes_tags : key => value
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
+
+resource "aws_iam_role" "eks_workers" {
+  name = "eks-node-group-example"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_workers.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_workers.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_workers.name
+}
