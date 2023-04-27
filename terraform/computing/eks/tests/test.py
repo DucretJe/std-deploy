7923da2d0432eba1a5e2186a4097419066cb32ca
DucretJe <jdiphone34@gmail.com>
2023-04-27 20:01:43 +0200

    Add python test

---
+++ terraform/computing/eks/tests/test.py
@@ -0,0 +1,98 @@
+import argparse
+
+import boto3
+import requests
+from botocore.exceptions import ClientError
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--region", required=True, help="AWS region")
+    parser.add_argument("--vpc-id", required=True, help="VPC ID")
+    parser.add_argument("--security-group-id", required=True, help="Security group ID")
+    parser.add_argument(
+        "--subnet-ids",
+        required=True,
+        nargs="+",
+        help="List of subnet IDs",
+    )
+    parser.add_argument("--cluster-name", required=True, help="EKS cluster name")
+    parser.add_argument("--worker-group-name", required=True, help="Worker group name")
+    parser.add_argument("--test-service-url", required=True, help="Test service URL")
+    return parser.parse_args()
+
+
+class TestFailed(Exception):
+    pass
+
+
+def test_eks_cluster_exists(region_name, cluster_name):
+    try:
+        session = boto3.Session()
+        eks_client = session.client("eks", region_name=region_name)
+        response = eks_client.describe_cluster(name=cluster_name)
+        assert (
+            response["cluster"]["name"] == cluster_name
+        ), f"EKS cluster {cluster_name} does not exist"
+        print(f"EKS cluster {cluster_name} exists")
+    except ClientError as e:
+        raise TestFailed(f"Failed to test EKS cluster {cluster_name}: {e}")
+
+
+def test_worker_group_exists(region_name, cluster_name, worker_group_name):
+    try:
+        session = boto3.Session()
+        eks_client = session.client("eks", region_name=region_name)
+        response = eks_client.describe_nodegroup(
+            clusterName=cluster_name, nodegroupName=worker_group_name
+        )
+        assert (
+            response["nodegroup"]["nodegroupName"] == worker_group_name
+        ), f"Worker group {worker_group_name} does not exist"
+        print(f"Worker group {worker_group_name} exists")
+    except ClientError as e:
+        raise TestFailed(f"Failed to test worker group {worker_group_name}: {e}")
+
+
+def test_test_service_response(test_service_url):
+    try:
+        if not test_service_url.startswith(("http://", "https://")):
+            test_service_url = f"http://{test_service_url}"
+        response = requests.get(test_service_url)
+        assert (
+            response.status_code == 200
+        ), f"Test service returned a {response.status_code} status code"
+        assert "It works!" in response.text, f"Test service returned: {response.text}"
+        print("Test service returned a 200 status code and 'It works!'")
+    except Exception as e:
+        raise TestFailed(f"Failed to test test service: {e}")
+
+
+def run_test(test_func, test_args):
+    try:
+        test_func(*test_args)
+    except TestFailed as e:
+        print(f"ERROR: {e}")
+        return False
+    return True
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    test_cases = [
+        (test_eks_cluster_exists, [args.region, args.cluster_name]),
+        (
+            test_worker_group_exists,
+            [args.region, args.cluster_name, args.worker_group_name],
+        ),
+        (test_test_service_response, [args.test_service_url]),
+    ]
+
+    has_errors = False
+    for test_func, test_args in test_cases:
+        if not run_test(test_func, test_args):
+            has_errors = True
+
+    if has_errors:
+        exit(1)
